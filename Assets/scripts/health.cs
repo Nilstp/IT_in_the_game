ef188f065ad18daa9ee59260d679a90f530cdd5c
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-12-07 14:16:38 +0100

    health collision with playercomponent

--- Assets/scripts/health.cs
+++ Assets/scripts/health.cs
@@ -7,21 +7,43 @@ public class health : MonoBehaviour
     [SerializeField]
     float playerHealth;
 
+    [SerializeField]
+    float timeUntillRegen;
+
+
+    [SerializeField]
+    float timer;
     void Start()
     {
-        
+
     }
+
+
 
     void Update()
     {
-        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            if (playerHealth != 2)
+                playerHealth = 2;
+        }
     }
+
+ 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag.Equals("enemy")) {
+        Debug.Log("verev");
+        if (collision.transform.tag.Equals("enemy"))
+        {
+            timer = timeUntillRegen;
             playerHealth -= 1;
-            if (playerHealth < 0) {
+            if (playerHealth < 0)
+            {
                 Destroy(this.gameObject);
             }
         }
